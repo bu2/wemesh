2ffc658e7fafd4813e397beab612c392f648ed90
Bruno Le Hyaric <bu2github@gmail.com>
2016-08-02 23:00:07 +0200

    Enable Python in Paraview.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ JOBS=4
 cd paraview
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$ROOT -DMACOSX_APP_INSTALL_PREFIX=$ROOT -DPARAVIEW_QT_VERSION=5 -DQt5_DIR=$QT5ROOT/lib/cmake/Qt5/ -DQt5Core_DIR=$QT5ROOT/lib/cmake/Qt5Core/ .. 2>&1 | tee cmake.log
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$ROOT -DMACOSX_APP_INSTALL_PREFIX=$ROOT -DPARAVIEW_QT_VERSION=5 -DQt5_DIR=$QT5ROOT/lib/cmake/Qt5/ -DQt5Core_DIR=$QT5ROOT/lib/cmake/Qt5Core/ -DPARAVIEW_ENABLE_PYTHON=ON .. 2>&1 | tee cmake.log
 make -j$JOBS 2>&1 | tee make.log
 make install 2>&1 | tee make_install.log
 cd ../..
