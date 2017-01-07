96a1afef8a37f8f657390b04596102c34b3dd3be
Bruno Le Hyaric <bu2github@gmail.com>
2017-01-07 20:28:29 +0100

    Update Paraview and PCL code then rebuild successfully (with Qt5.7)

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 ROOT=$PWD
 
-# QT5ROOT=$HOME/lab/qt-5.6/5.6/clang_64
+QT5ROOT=$HOME/lab/qt-5.7/5.7/clang_64
 
 JOBS=4
 
 cd paraview
 mkdir -p build
 cd build
-# cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$ROOT -DMACOSX_APP_INSTALL_PREFIX=$ROOT -DPARAVIEW_QT_VERSION=5 -DQt5_DIR=$QT5ROOT/lib/cmake/Qt5/ -DQt5Core_DIR=$QT5ROOT/lib/cmake/Qt5Core/ -DPARAVIEW_ENABLE_PYTHON=ON .. 2>&1 | tee cmake.log
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$ROOT -DMACOSX_APP_INSTALL_PREFIX=$ROOT -DPARAVIEW_ENABLE_PYTHON=ON .. 2>&1 | tee cmake.log
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$ROOT -DMACOSX_APP_INSTALL_PREFIX=$ROOT -DPARAVIEW_QT_VERSION=5 -DQt5_DIR=$QT5ROOT/lib/cmake/Qt5/ -DQt5Core_DIR=$QT5ROOT/lib/cmake/Qt5Core/ -DPARAVIEW_ENABLE_PYTHON=ON .. 2>&1 | tee cmake.log
+# cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$ROOT -DMACOSX_APP_INSTALL_PREFIX=$ROOT -DPARAVIEW_ENABLE_PYTHON=ON .. 2>&1 | tee cmake.log
 make -j$JOBS 2>&1 | tee make.log
 make install 2>&1 | tee make_install.log
 cd ../..
